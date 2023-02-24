09efd9f4aa009ebaf4c10b0891b87315cbb22cda
duonghuupha <duonghuupha@hotmail.com.vn>
2023-02-24 13:11:34 +0700

    sa

--- Bai_test/Phan_1_muc_4_bai_1.py
+++ Bai_test/Phan_1_muc_4_bai_1.py
@@ -2,3 +2,17 @@ A = [1, 2, 3, 4, 5]
 A.append(6)
 B = A
 print(B)
+
+tong = 0
+for item in B:
+    tong += int(item)
+print(tong)
+
+C  = []
+for item in B:
+    if int(item) % 2 == 0:
+        C.append(int(item))
+print(C)
+
+del(C[0])
+print(C)
